b6458bcc7edec1a94aa15f92efe402e5ec37e9ee
Q2099 <qaisarali@hotmail.com>
2026-04-28 16:06:12 +0100

    Update addpackage.js

--- Odysseus/addpackage.js
+++ Odysseus/addpackage.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+  if (!window.location.pathname.toLowerCase().includes("checkout.aspx")) {
+    return; // stop if not checkout page
+  }
+
   const table = document.querySelector("table.package-itinerary.mProItineraryTable");
 
   if (table) {
     table.insertAdjacentHTML("beforebegin", `
       <div class="my-custom-block">
-        <h3>Injected Content</h3>
+        <h3>Injected on Checkout Only</h3>
       </div>
     `);
   }
